006c0882f52a804e7bcc620534d91fc4aac6f27c
axptwig <twig@deltaphicrows.com>
2015-12-31 15:26:09 -0800

    fixed send to node

--- node.py
+++ node.py
@@ -1,8 +1,6 @@
 import SocketServer
 from threading import Thread
 from sys import argv
-from calendar import *
-from time_table import *
 import socket
 node = None
 
@@ -10,15 +8,14 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
+
+        self.request.sendall("ACK") #replace with some appropriate responce...
         if node:
             node.receive(self.data)
 
 class Node():
-    ips = []
-    def __init__(self, _id):
-        self.id = _id
-        self.ip = Node.ips[self.id]
-
+    def __init__(self, ip):
+        self.ip = ip
         listener = SocketServer.TCPServer((self.ip, 6000), MyTCPHandler)
         self.thread = Thread(target = listener.serve_forever)
         self.thread.start()
@@ -32,34 +29,38 @@ class Node():
         print "Received Data"
         print data
 
-    def send(self, _id):
+    def send(self, ip):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             data = "data"
             print "Sending Data from client"
             # Connect to server and send data
-            sock.connect((Node.ips[_id], 6000))
+            sock.connect((ip, 6000))
             sock.sendall(data + "\n")
 
             # Receive data from the server and shut down
             received = sock.recv(1024)
-            # Add To EntrySet
-        except :
-            pass
-            # Node Down cancel conflict
+            print "ACK: " + received
         finally:
             sock.close()
 
     # Check if a node has a certain event
-    def has_event(event, node_id):
+    def has_event(self, event, node_id):
         return self.table.get(node_id, event.node) >= event.time
 
-    def send_to_node(node_id):
+    def send_to_node(self, node_id):
         partial = []
         for event in self.events:
             if not self.has_event(event, node_id)
                 partial.append(event)
 
+        data = {
+            'table': self.table,
+            'events': partial,
+        }
+
+        self.send(json.dumps(data))
+
 if __name__ == "__main__":
-    Node.ips = open('ip', 'r').read().split("\n")[0:4]
     node = Node(argv[1])
+    dic = EntrySet()
