7fd1d6fb1a3a2bd2a0b926be40972529d6d6414c
axptwig <twig@deltaphicrows.com>
2015-12-31 17:12:51 -0800

    fixed node / added interface

--- node.py
+++ node.py
@@ -18,13 +18,13 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 class Node():
     ips = []
     def __init__(self, _id):
-        self.id = int(_id)
+        self.id = _id
         self.ip = Node.ips[self.id]
 
         listener = SocketServer.TCPServer((self.ip, 6000), MyTCPHandler)
         self.thread = Thread(target = listener.serve_forever)
         self.thread.start()
-        dic = calendar.EntrySet()
+        self.entry_set = calendar.EntrySet()
         if (os.path.isfile("log.dat")):
             dic.create_from_log()
 
@@ -40,23 +40,21 @@ class Node():
         # unserialize the data, somehow
         data = json.loads(raw)
 
-        if data['type'] == "failure":
-            rec_failure(data)
-        else:
-            new_table = UNSERIALIZE_TABLE(data['table'])
-            new_events = UNSERIALIZE_EVENTS(data['events'])
+        new_table = TimeTable.load(json.loads(data['table']))
+        events = data['events']
 
-            # For all events this node doesn't have, make modifications
-            for event in new_events:
-                if not self.has_event(event, self.id):
-                    res = event.apply(self.entry_set)
-                    if res:
-                        self.events.append(event)
-                    elif event.type == MessageTypes.Insert:
-                        send_failure(event)
+        new_events =[]
+        for event in events:
+            new_events.append( Event.load(json.loads(event) ))
 
+        # For all events this node doesn't have, make modifications
+        for event in new_events:
+            if not self.has_event(event, self.id):
+                res = event.apply(self.entry_set)
+                if res:
+                    self.events.append(event)
 
-            self.table.sync(new_table)
+        self.table.sync(new_table)
 
     def send(self, _id):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -76,19 +74,6 @@ class Node():
         finally:
             sock.close()
 
-    def send_failure(self, event):
-        #grab id from event
-
-        print("Sending Failure command")
-        data = {
-            'type': 'failure',
-            'event': event.to_JSON()
-        }
-        json.dumps(data)
-    def rec_failure(self, data):
-        data = json.loads(data)
-        event = Event.load(json.loads(data['event']))
-
     # Check if a node has a certain event
     def has_event(self,event, node_id):
         return self.table.get(node_id, event.node) >= event.time
@@ -97,10 +82,10 @@ class Node():
         partial = []
         for event in self.events:
             if not self.has_event(event, node_id):
-                partial.append(event)
+                partial.append(event.to_JSON())
 
         data = {
-            'table': self.table,
+            'table': self.table.to_JSON(),
             'events': partial,
         }
 
@@ -109,3 +94,21 @@ class Node():
 if __name__ == "__main__":
     Node.ips = open('ip', 'r').read().split("\n")[0:4]
     node = Node(argv[1])
+
+    print "[v] View Appointments"
+    print "[a] Add Appointment"
+    print "[d] Delete Appointment"
+
+    resp = raw_input("Choice: ").lower()
+    if resp == 'v':
+        for entry in node.entry_set:
+            print entry
+
+    elif resp == 'a':
+        pass
+
+    elif resp == 'd':
+        resp = raw_input("Enter Appointment number: ")
+
+
+        
